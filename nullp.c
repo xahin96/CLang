0063509b7ba54c875ed1256bdb7be02806a03dac
zahin <xahin96@gmail.com>
2024-02-15 14:40:17 -0500

    NULL pointer test

--- nullp.c
+++ nullp.c
@@ -1,23 +1,18 @@
 #include <stdio.h> 
 #include <stdlib.h>
-
-int * division(int a, int b)
-{
+int * division(int a, int b){
     int d; 
     if(b == 0)
         return NULL;
     else
-        d = a / b;
-    printf("\nThe result of the integer division is %d\n",d); 
+        return (int *) (a / b);
 }
-
-main()
-{
-    int * ptr = division(20, 0); 
-    printf("\n * ptr = %d", ptr);
+int main(){
+    int * ptr = division(20, 0);
+    printf("\n ptr = %d", ptr);
+//    printf("\n ptr address = %d", ptr);
     if(ptr == NULL)
         printf("\nDivision unsuccessful\n");
-
-    int choco = NULL;
+    int * choco = NULL;
     printf("\n choco: %d \n", choco);
 }
